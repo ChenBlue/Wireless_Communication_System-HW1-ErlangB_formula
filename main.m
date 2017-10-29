ab2816e205b128a8f03ac5bd79962110f76b60be
ChenBlue <bob840806@gmail.com>
2017-10-29 23:41:16 +0800

    Modify main.m - Calculate Rho for Block prob.=1%,3%,5%,10%; Channel number:1~20

--- main.m
+++ main.m
@@ -1,2 +1,11 @@
-erlangB(1, 2)
-erlangBdiff(1,2)
+rho_ans = [0.01 1 erlangRho(0.01,1)];
+for B = [0.01 0.03 0.05 0.1]
+    for m = 1:20
+        if m==1 & B==0.01
+            continue
+        end
+        rho_ans = cat(1, rho_ans, [B m erlangRho(B,m)]);
+    end        
+end
+
+rho_ans
